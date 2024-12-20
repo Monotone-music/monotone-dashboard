1ca098b22152015dc5c5e863b3af11305de668c7
vuhaianh0907 <vuhaianh0907@gmail.com>
2024-12-20 15:03:15 +0700

    fix: history UI

--- src/main.tsx
+++ src/main.tsx
@@ -14,12 +14,10 @@ import AuthPage from "./page/Auth/AuthPage";
 import { Toaster } from "./components/ui/toaster";
 import RootLayout from "./layout/Root/RootLayout";
 import HomePage from "./page/Admin/Home/HomePage";
-import AccountsManagement from "./page/Admin/Accounts/AccountsManagement";
-import ArtistAccount from "./page/Admin/Accounts/Artist/ArtistAccount";
-import UploaderPage from "./page/Uploader/UploaderPage";
 import ApprovePage from "./page/Approve/ApprovePage";
 import { useEffect } from "react";
 import { setNavigate } from "./service/apiClient";
+import PaymentHistoryPage from "./page/PaymentHistory/PaymentHistoryPage";
 // import router from "./util/Router";
 
 const queryClient = new QueryClient({
@@ -50,13 +48,9 @@ const router = createBrowserRouter([
     element: <NavigationWrapper><RootLayout /></NavigationWrapper>,
     children: [
       { path: "overview", element: <HomePage /> },
-      {
-        path: "accounts-management",
-        element: <AccountsManagement />,
-        children: [{ path: "artist", element: <ArtistAccount /> }],
-      },
-      { path: "uploader", element: <UploaderPage/> },
       { path: "approve", element: <ApprovePage /> },
+      { path: "history", element: <PaymentHistoryPage /> },
+
     ],
   },
 ]);
